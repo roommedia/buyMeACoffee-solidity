606dd2f680e29143a78cf39c89bc5c4173c0820c
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2022-05-10 11:23:12 -0300

    Initial commit

---
+++ contracts/BuyMeACoffee.sol
@@ -0,0 +1,57 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract BuyMeACoffee {
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+
+    Memo[] memos;
+
+    address payable owner;
+
+    // deploy logic!
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function buyCoffee(string memory _name,string memory _message) public payable  {
+        require(msg.value > 0,"can`t buy coffee with 0 eth");
+
+        // Add the memo to storage!
+        memos.push(
+            Memo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+            )
+        );
+
+        // Emit a log event when a new memo is created!
+        emit NewMemo(
+            msg.sender,
+            block.timestamp,
+            _name,
+            _message
+        );
+    }
+
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+
+    function getMemos() public view returns(Memo[] memory){
+        return memos;
+    }
+}

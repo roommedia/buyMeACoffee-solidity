45f913969d54fb2216f73468dd1244eeada60875
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2022-05-11 15:31:08 -0300

    a

---
+++ scripts/buy-coffee.js
@@ -0,0 +1,49 @@
+
+const hre = require("hardhat");
+
+// return the Ethere balance of a given address.
+async function getBalance(address) {
+const balanceBigInt = await hre.waffle.provider.getBalance(address);
+return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+
+// Logs the Ether balances for a list of addresses.
+async function printBalances(addresses) {
+  let idx = 0;
+  for(const address of addresses) {
+    console.log(`Address ${idx} balance: `, await getBalance(address))
+    idx++
+  }
+}
+
+// Logs the memos stored on-chain from coffee purchases
+async function printMemos(memos) {
+  for (const { timestamp, tipper, from: tipperAddress, message } of memos) {
+    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`)
+  }
+}
+
+async function main() {
+  const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
+
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
+  await buyMeACoffee.deployed();
+
+  console.log("buyMeACoffee deployed to ", buyMeACoffee.address);
+
+  const addresses = [owner.address, tipper.address, buyMeACoffee.address];  
+  console.log("== start ==");
+  await printBalances(addresses);
+
+  const tip = {value: hre.ethers.utils.parseEther("1")};
+  buyMeACoffee.connect(tipper)
+} 
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
